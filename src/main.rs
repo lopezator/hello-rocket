9c3aca2a9831e97889180206ca5fabe08c9374a0
David López <dlopbec@gmail.com>
2017-07-30 14:20:01 +0200

    Hello world in rocket!

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@
 
 extern crate rocket;
 
-#[get("/")]
+#[get("/world")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/hello", routes![index]).launch();
 }
+
+/*
+Notas:
+La función index se carga mediante el path "/world".
+Después se monta el path "/hello" sobre la ruta index (que corresponde al path "/world").
+La el path completo resultante será /hello/world
+*/
 
